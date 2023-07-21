5ad27fed23ae6534e18d5e6e98222ec80bbd1d31
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-07-21 12:01:53 +0200

    Add reaction for html post

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rocket::serde::{Deserialize, json::Json};
+use std::{fs::OpenOptions, io::Write};
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +8,27 @@ fn index() -> &'static str {
     "Hello World!"
 }
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str,
+}
+
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("Unable to open tasks.txt");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks.write(task_item_bytes).expect("Cant write to tasks.txt");
+    "Task added successfully"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, add_task])
 }
