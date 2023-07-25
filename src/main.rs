1c4504415578099ece848d8874a645fc020db28c
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-07-25 18:19:58 +0200

    Add inpput text field

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,19 @@ fn main() {
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 0);
+    let (value, set_value) = create_signal(cx, String::from(""));
 
     let clear = move |_ev| set_count.set(0);
     let decrement = move |_ev| set_count.update(|count| *count -= 1);
     let increment = move |_ev| set_count.update(|count| *count += 1);
+    let enter_txt = move |ev| {
+        let txt = event_target_value(&ev);
+        if let Ok(data) = txt.parse::<i32>() {
+            set_value.set(txt);
+            log!("Entered text: \"{}\"", value.get());
+            set_count.set(data);
+        }
+    };
     
     view! {
         cx,
@@ -19,6 +28,7 @@ fn App(cx: Scope) -> impl IntoView {
             <button on:click=decrement>"-1"</button>
             <span>"Value: " {move || count.get().to_string()} "!"</span>
             <button on:click=increment>"+1"</button>
+            <input type="text" on:input=enter_txt></input>
         </div>
     }
 }
