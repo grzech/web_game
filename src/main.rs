fa2b7637e91af339bc8bffa108c5fd1e5accf7d2
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-08-12 10:37:05 +0200

    Add styler.css file to configure: - colors - alignment

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,43 @@
 use leptos::*;
 
-#[derive(PartialEq, std::fmt::Debug)]
-enum Game {
-    None,
-    Snake,
-    PingPong,
-}
-
-static mut SELECTED_GAME: Game = Game::None;
-
 fn main() {
-    mount_to_body(|cx| view!{ cx, <App/>> })
+    mount_to_body(|cx| view!{ cx, <App/> })
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    unsafe{log!("App: Game = {:?}", SELECTED_GAME);};
+    let (game_started, set_game_started) = create_signal(cx, false);
+    let start_game = move |_ev| { set_game_started.set(true) };
     view! {
         cx,
-        <div align="center">
+        <div id="Web Game">
+            <h4>"Player name"</h4>
+            <h3><input type="text"></input></h3>
+            <h3><button on:click=start_game>"Start"</button></h3>
             <Show
-                when= move || { unsafe{ SELECTED_GAME == Game::None } }
-                fallback=|cx| view! { cx, <Player/> }
+                when= move || { game_started.get() }
+                fallback=|cx| view! { cx, <> }
             >
-                <MainMenu/>
+                <Game/>
             </Show>
         </div>
     }
     
 }
 
-
 #[component]
-fn MainMenu(cx: Scope) -> impl IntoView {
-    let snake = move |_ev| unsafe { SELECTED_GAME = Game::Snake; };
-    let ping_pong = move |_ev| unsafe { SELECTED_GAME = Game::PingPong; };
-    unsafe{log!("MainMenu: Game = {:?}", SELECTED_GAME);};
+fn Game(cx: Scope) -> impl IntoView {
     view! {
         cx,
         <div align="center">
-            <h1>"Select the game"</h1>
-            <h4><button on:click=snake>"Snake"</button></h4>
-            <h4><button on:click=ping_pong>"Ping-Pong"</button></h4>
-        </div>
-    }
-
-}
-
-#[component]
-fn Player(cx: Scope) -> impl IntoView {
-    unsafe{log!("Player: Game = {:?}", SELECTED_GAME);};
-    view! {
-        cx,
-        <div align="center">
-            <h1>"Give your name"</h1>
-            <h4><input type="text"></input></h4>
+            <h2>"#####################<br>
+            #                   #<br>
+            #                   #<br>
+            #                   #<br>
+            #                   #<br>
+            #                   #<br>
+            #                   #<br>
+            #####################<br>"</h2>
         </div>
     }
 }
